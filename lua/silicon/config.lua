22fc58f4f8e73121439638bd7fbf96bb82b1a43a
vlaw <cashislord@gmail.com>
2025-04-24 17:59:40 +0800

    refactor: clean up and simplify
    - 删除 theme 相关功能 (基于vim生成theme, cache)
    - 简化配置参数, 统一到 config 文件

--- lua/silicon/config.lua
+++ lua/silicon/config.lua
@@ -1,29 +1,21 @@
-local config = {}
+local M = {}
 
-config.opts = {
-	theme = "auto",
-	output = "SILICON_${year}-${month}-${date}_${time}.png",
-	bgColor = vim.g.terminal_color_5,
-	bgImage = "", -- path to image, must be png
-	roundCorner = true,
-	windowControls = true,
-	lineNumber = true,
-	font = "monospace",
-	lineOffset = 1, -- no idea what this is
-	linePad = 2, -- padding between lines
-	padHoriz = 80, -- Horizontal padding
-	padVert = 100, -- vertical padding
-	shadowBlurRadius = 10,
-	shadowColor = "#555555",
-	shadowOffsetX = 8,
-	shadowOffsetY = 8,
-	gobble = false,
-	debug = false,
+---@class SiliconOpts
+local default = {
+  theme = "Dracula",
+  output = "SILICON_${year}-${month}-${date}_${time}.png",
+  gobble = false,
+  silicon_args = nil,
+  debug = false,
 }
 
 --- @param opts table
-config.setup = function(opts)
-	config.opts = vim.tbl_deep_extend("force", config.opts, opts or {})
+M.setup = function(opts)
+  M.opts = vim.tbl_deep_extend(
+    "force",
+    default,
+    opts or {}
+  )
 end
 
-return config
+return M
